2b39dfba0717076f8285701c604283d1b7cd1721
FerLS <fernando.alvarezr@udc.es>
2024-02-25 23:52:35 +0100

    fix democard

--- src/components/DemoCard.tsx
+++ src/components/DemoCard.tsx
@@ -15,32 +15,37 @@ interface DemoCardProps {
     title?: React.ReactNode;
     description?: string;
     link?: string;
-    className?: string;
 }
 
-export function DemoCard({ title = <p>Title</p>, description = "Card Description", link = "null", className }: DemoCardProps) {
+export function DemoCard({ title = <p>Title</p>, description = "Card Description", link = "null" }: DemoCardProps) {
+
+
+
 
     return (
-        <div className={`transition-all ease-in-out hover:scale-110 dark:hover:light hover:dark duration-300 hover:z-10 overflow-hidden shadow-lg ${className}`}>
 
 
-            <div className={`transition-all ease-in-out hover:scale-110 dark:hover:light hover:dark duration-300 hover:z-10 overflow-hidden shadow-lg `}>
+        <div className={`transition-all ease-in-out hover:scale-110 dark:hover:light hover:dark duration-300 hover:z-10 overflow-hidden shadow-lg `}>
 
-                <Card className="h-full border-2">
-                    <CardHeader>
-                        <CardTitle>{title}</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <CardDescription>{description}</CardDescription>
-                    </CardContent>
-                    <CardFooter className="flex">
-                        <Button
-                            onClick={() => {
-                                window.location.href = link
-                            }}
-                        >Try  &rarr;</Button>
-                    </CardFooter>
-                </Card>
-            </div >
-            )
+            <Card className="h-full border-2">
+                <CardHeader>
+                    <CardTitle>{title}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <CardDescription className="h-20">{description}</CardDescription>
+                </CardContent>
+                <CardFooter >
+                    <Button
+                        onClick={() => {
+
+                            window.location.href = link
+                        }}
+
+                    >Try  &rarr;</Button>
+
+                </CardFooter>
+            </Card>
+        </div >
+
+    )
 }
