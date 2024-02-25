62c4ebe2baace532797d7c7ede9cbfc86174d609
Fernando Álvarez <114153352+FerLS@users.noreply.github.com>
2024-02-25 18:48:32 +0100

    Update ModeToggle.tsx

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -5,42 +5,22 @@ import { Moon, Sun, ActivitySquare } from "lucide-react"
 import { useTheme } from "next-themes"
 
 import { Button } from "@/components/ui/button"
-import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 
 export function ModeToggle() {
-    const { setTheme } = useTheme()
+    const { setTheme, theme } = useTheme()
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon" className=" border-background dark:border-primary border-2  mix-blend-difference bg-transparent   hover:bg-transparent">
-                    <Sun color="background" className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button
 
-                    <span className="sr-only">Toggle theme</span>
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" >
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                    <Sun className="h-4 w-4 mr-2" />
+            variant="outline" size="icon" className=" border-background dark:border-primary border-2  mix-blend-difference bg-transparent   hover:bg-transparent"
+            onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+        >
+            <Sun color="background" className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
 
-                    Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                    <Moon className="h-4 w-4 mr-2" />
 
-                    Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                    <ActivitySquare className="h-4 w-4 mr-2" />
-                    System
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        </Button>
+
+
     )
 }
