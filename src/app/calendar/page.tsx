a2b311daf354c3dd6a0008b56bf1b2b555328129
FerLS <fernando.alvarezr@udc.es>
2024-04-22 12:38:07 +0200

    calendar mini fix

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,10 +1,12 @@
 
+
 export default function Calendar() {
+    const currentDate = new Date();
 
     return (
         <div className='h-l space-y-5'>
             <h1 className='text-4xl font-bold text-center px-10'>
-                😲 HOY ES {new Date().toLocaleString("es", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })} 😲
+                😲 HOY ES {currentDate.toLocaleString("es", { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })} 😲
             </h1>
             <h2 className='text-2xl text-center'>
                 Nah que locura de calendario
